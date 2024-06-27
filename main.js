b27edb86a8ef4f7aa5ec8677a9ed83e60de18286
ahmed <ahmedmijdo2@gmail.com>
2024-06-27 17:14:03 +0300

    Remove Some comment

--- main.js
+++ main.js
@@ -1,28 +1,4 @@
-// Calculate Program
 
-// const display = document.getElementById("display");
-
-
-// function click(input){
-//      display.value += input;
-// }
-
- 
-
-// function clear() {          
-//      display.value = "";  
-// }
-
-
-
-// function calculate(){
-//     try{
-//      display.value = eval(display.value);
-//     }
-//     catch(error){ 
-//      display.value = "Error";
-//     }
-// }
 
 function equ(){
      var eq = eval(document.getElementById('display').value)
